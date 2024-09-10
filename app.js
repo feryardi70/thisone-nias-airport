e47fc0d032ab0e96455545e8489b37f38c76386f
feryardi70 <fery.ardiansyah94747@gmail.com>
2024-09-10 23:52:48 +0700

    add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const { body, validationResult, check } = require("express-validator");
+const methodOverride = require("method-override");
+
+require("./db");
+// const Book = require("./model/contact.js");
+// const Comment = require("./model/comment.js");
+const Departure = require("./model/departure.js");
+
+const app = express();
+const port = process.env.PORT || 10000;
+
+app.use(methodOverride("_method"));
+
+//setup ejs
+app.set("view engine", "ejs");
+app.use(expressLayout);
+
+//
+app.use(express.json());
+
+// built-in middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", async (req, res) => {
+  const departures = await Departure.find();
+  res.render("index", { layout: "Layouts/none", departures });
+});
+
+app.get("/departure", async (req, res) => {
+  const departures = await Departure.find().sort({ departtime: 1 });
+  res.render("departure", { layout: "Layouts/departure-layout", departures });
+});
+
+// procesc add depart
+app.post("/", (req, res) => {
+  Departure.insertMany(req.body),
+    (error, result) => {
+      if (error) {
+        console.log("gagal memasukkan data ke database");
+      } else {
+        console.log(result.status.json());
+      }
+      //res.redirect(301, "/contact");
+    };
+  res.redirect("/");
+});
+
+app.get("/delete/:airline", async (req, res) => {
+  const departure = await Departure.findOne({ airline: req.params.airline });
+  if (!departure) {
+    res.status(404);
+    res.send("<h1>gagal menghapus jadwal penerbangan</h1>");
+  } else {
+    Departure.deleteOne({ _id: departure._id }).then((result) => {
+      console.log("berhasil terhapus");
+    });
+  }
+  res.redirect("/");
+});
+
+app.get("/edit/:airline", async (req, res) => {
+  const departure = await Departure.findOne({ airline: req.params.airline });
+  res.render("editdeparture", { layout: "Layouts/none", departure });
+});
+
+app.put("/", (req, res) => {
+  //console.log(req.body);
+  Departure.updateOne(
+    { _id: req.body._id },
+    {
+      $set: {
+        airline: req.body.airline,
+        flightnumber: req.body.flightnumber,
+        destination: req.body.destination,
+        departdate: req.body.departdate,
+        departtime: req.body.departtime,
+        gate: req.body.gate,
+        remark: req.body.remark,
+      },
+    }
+  ).then((result) => {
+    console.log("berhasil mengubah data");
+  });
+  res.redirect("/");
+});
+
+app.get("/add", (req, res) => {
+  res.render("adddeparture", { layout: "Layouts/none" });
+});
+
+app.use("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>404 <br>Halaman tidak ditemukan</h1>");
+});
+
+app.listen(port, () => {
+  console.log(`app is listening on port ${port}..`);
+});
